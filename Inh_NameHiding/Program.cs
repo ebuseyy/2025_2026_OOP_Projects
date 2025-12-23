55d5c48b58441ba76cca3f802c18c9f82e312e8f
ebuseyy <eseyyarer@gmail.com>
2025-12-23 11:19:09 +0300

    name hiding

---
+++ Inh_NameHiding/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Inh_NameHiding
+{
+    class X
+    {
+        protected int a;
+        public X(int a)
+        {
+            this.a = a;
+        }
+        public X()
+        {
+
+        }
+        public int A
+        { 
+            get 
+            {
+                Console.WriteLine("X sınıfı");
+                return a; 
+            } 
+        }   
+    }
+    class Y : X
+    {
+        protected int b;
+        public Y(int a)
+        {
+            this.b = a;
+        }
+        public int A
+        {
+            get
+            {
+                Console.WriteLine("Y sınıfı");
+                return b;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Y y = new Y(5);
+            int deneme = y.A;
+            Console.WriteLine(deneme);
+        }
+    }
+}
